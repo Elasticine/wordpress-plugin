22413214d34f853a166dbd4811c6bcea4deae837
hpwfranks <hpwfranks@googlemail.com>
2016-08-16 13:16:56 +0100

    Initial commit

---
+++ elasticine-artists.php
@@ -0,0 +1,378 @@
+<?php
+/**
+ * Plugin Name: Elasticine 
+ * Description: Pulls in data from the Elasticine platform.
+ * Version: 0.0.2
+ * Author: Henry Franks
+ *
+ * 
+    Elasticine wordpress plugin
+    Copyright (C) 2016 Elasticine Ltd. 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+require_once(ABSPATH . 'wp-admin/includes/taxonomy.php'); 
+
+/********************************************
+ * Installation hooks and definitions
+ ********************************************/
+register_activation_hook(__FILE__, 'elasticine_install');
+register_deactivation_hook(__FILE__, 'elasticine_uninstall');
+
+function elasticine_install()
+{
+		//Create option database fields
+		
+		//What is the company name using the wordpress plugin?
+		add_option('elasticine_artists_company_name', 'Elastic Artists Ltd.', '', 'yes');
+		
+		//Do we want to maintain a post category for each artist?
+		add_option('elasticine_artists_maintain_categories', 'yes', '', 'no');
+		
+		//Where do we want to get the data from?
+		add_option('elasticine_artists_base_url', 'system.elasticine.net/api', '', 'yes');
+}
+
+function elasticine_uninstall()
+{
+		delete_option('elasticine_artists_company_name');
+		delete_option('elasticine_artists_maintain_categories');
+		delete_option('elasticine_artists_base_url');
+}
+
+/*******************************************
+	Register the admin menu with wordpress
+********************************************/
+add_action( 'admin_menu', 'elasticine_artists_menu' );
+
+function elasticine_artists_menu() {
+	add_options_page( 'Elasticine options', 'Elasticine', 'manage_options', 'elasticine-options', 'elasticine_options' );
+	register_setting('elasticine_settings', 'elasticine_artists_base_url');
+	register_setting('elasticine_settings', 'elasticine_artists_company_name');
+	register_setting('elasticine_settings', 'elasticine_artists_maintain_categories');
+}
+
+function elasticine_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+?>
+	<div>
+		<h2>Elasticine Options</h2>
+		
+		<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options');
+				settings_fields( 'elasticine_settings' );
+		?>
+		
+		<table width="100%">
+				<tr valign="top">
+						<th scope="row">Your company name</th>
+								<td>
+										<input name="elasticine_artists_company_name" type="text" id="elasticine_artists_company_name""
+														value="<?php echo get_option('elasticine_artists_company_name'); ?>" />
+												This should be the exact company name you have set in the Elasticine Agency Management System.
+								</td>
+				</tr>
+		</table>
+		<hr/>
+		<table width="100%">
+				<tr valign="top">
+						<th scope="row">Elasticine API URL</th>
+								<td>
+										<input name="elasticine_artists_base_url" type="text" id="elasticine_artists_base_url""
+														value="<?php echo get_option('elasticine_artists_base_url'); ?>" />
+												Where to get your company data from. Do not change this unless absolutely necessary. 
+								</td>
+				</tr>
+		</table>
+		<hr/>
+		<table width="100%">
+				<tr valign="top">
+						<th scope="row">
+								<label>
+										<input name="elasticine_artists_maintain_categories" type="checkbox" id="elasticine_artists_maintain_categories"
+														value="yes" <?php if (get_option('elasticine_artists_maintain_categories') == "yes") { echo "checked='checked'"; }?>/>
+														Maintain artist post categories?
+								</label>
+						</th>
+								<td>
+												If you select this option, we will create a post category for every artist in your company, so you can see individual feeds of posts for each artist.
+								</td>
+				</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		
+		<p>
+		<input type="submit" value="<?php _e('Save Changes') ?>" />
+		</p>
+		
+		</form>
+		</div>
+<?php
+}
+
+/*****************************************************
+	Functions for getting data from elasticine API
+******************************************************/
+
+/**
+ * Returns the base URL for the elasticine API
+ */
+function elasticine_build_url()
+{
+	return 	"http://" . get_option('elasticine_artists_base_url') . "/json/" . sanitize_title(get_option('elasticine_artists_company_name'));
+}
+
+function elasticine_update_artists()
+{
+		$cachePeriod = 60 * 60 * 6;  //Update every 6 hours
+		$url = elasticine_build_url() . "/artists/";
+		$response = wp_remote_get($url);
+		$http_code = wp_remote_retrieve_response_code( $response );
+		if ($http_code == 200)
+		{
+			$body = wp_remote_retrieve_body($response);
+			set_transient('elasticine_artists', $body, $cachePeriod);
+		} else {
+			die("HTTP Error " . $http_code);
+		}
+}
+
+
+/**
+ *  Gets top-level info for all artists
+ */
+function elasticine_get_artists()
+{		
+		//Get it from the cache
+		$artists = get_transient('elasticine_artists');
+
+		//Not in the cache? Get it from the API
+		if ( $artists === false )
+		{
+			elasticine_update_artists();
+			$artists = get_transient('elasticine_artists');
+		}
+
+		$artists = json_decode($artists);
+
+		//Ensure the categories are up to date with the new info
+		elasticine_maintainCategories($artists);
+
+		return $artists;
+}
+
+/**
+ * Gets detailed information for a single artist
+ */
+function elasticine_get_artist($slug)
+{
+    $cachePeriod = 60 * 60 * 6;
+    $transient = 'elasticine_artist_' . $slug;
+	
+	//If it's in the cache, don't worry
+	$artist  = get_transient($transient);
+	if ($artist !== false) { return json_decode($artist); }
+	
+	//It's not in the cache, so grab a copy of the data from the API
+	$url = elasticine_build_url() . '/artist/' . $slug;
+	$response = wp_remote_get($url);
+	$http_code = wp_remote_retrieve_response_code( $response );
+	if ($http_code == 200)
+	{
+		$body = wp_remote_retrieve_body($response);
+		$artist = $body;
+		set_transient($transient, $body, $cachePeriod);
+	} else {
+		die("HTTP Error");
+	}
+		
+    //If called from do_action this return statement will be meaningless, but include it as we don't always
+	//want to call this function from do_action
+	return json_decode($artist);
+}
+
+/**
+ * Gets all shows for a company between the given dates in yyyy-mm-dd format
+ */
+function elasticine_get_shows($startDate, $endDate)
+{
+		$cachePeriod = 60 * 60 * 6;
+		$transient = "es_" . $startDate . "_" . $endDate;
+		$shows = get_transient($transient);
+		if ($shows !== false) { return; }
+
+		$url = elasticine_build_url() . '/shows/' . $startDate . "/" . $endDate;
+		
+		$response = wp_remote_get($url);
+		$http_code = wp_remote_retrieve_response_code( $response );
+		if ($http_code == 200)
+		{
+			$body = wp_remote_retrieve_body($response);
+			set_transient($transient, $body, $cachePeriod);
+		} else {
+			die("HTTP Error " . $http_code);
+		}
+}
+
+/**
+ * Gets all shows for an artist between the given dates in yyyy-mm-dd format
+ */
+function elasticine_get_shows_artist($artist, $startDate, $endDate)
+{
+		$cachePeriod = 60 * 60 * 6;
+		$transient = 'es_' . $artist . "_" . $startDate . "_" . $endDate;
+		$shows = get_transient($transient);
+		if ($shows !== false) { return; }
+		
+		$url = elasticine_build_url() . '/artist_shows/' . $startDate . "/" . $endDate . "/" . $artist;
+		$response = wp_remote_get($url);
+		$http_code = wp_remote_retrieve_response_code( $response );
+		if ($http_code == 200)
+		{
+			$body = wp_remote_retrieve_body($response);
+			set_transient($transient, $body, $cachePeriod);
+		} else {
+			die("HTTP Error " . $http_code);
+		}
+}
+
+/*****************************************************
+	Register tags that can be used in themes
+******************************************************/
+
+add_action('elasticine_artist', 'elasticine_get_artist');
+add_action('elasticine_artists', 'elasticine_get_artists');
+add_action('elasticine_shows', 'elasticine_get_shows', 10, 2);
+add_action('elasticine_shows_artist', 'elasticine_get_shows_artist', 10, 3);
+
+
+/*****************************************************
+ * Functions for maintaining the set of artist post categories
+ *************************************************/
+
+ function elasticine_maintainCategories($artists)
+ {
+		//All artist categories are a child of an "Artist" category. Make sure we have that!
+		$parentID = wp_create_category('Artists');
+		
+		if (!elasticine_isCategoryUpdateNeeded($artists, $parentID)) { return; }
+		
+		$artistCategories = get_categories(array('child_of' => $parentID, 'hide_empty' => 0));
+		
+		//Create an O(1) data structure for artist names from the API, so that we can check which
+		//don't exist without inducing an O(n^2) search. Arrays in PHP are hashtables so key lookups are O(1)
+		//If a category exists which doesn't exist  in this array, then we need to delete that category as that artist
+		//is no longer represented by the company. 
+		$artistNames = array();
+		
+		//The create_category function nicely doesn't duplicate a category if it already exists,
+		//so we can just create a category for each artist
+		foreach($artists as $artist)
+		{
+				$newCatID = wp_create_category($artist->name, $parentID);
+				
+				//Populate the hashtable
+				$artistNames[$artist->name] = 1;
+		}
+		
+		//Now go through each category and delete any that don't exist from the API
+		foreach($artistCategories as $category)
+		{
+				if (!array_key_exists($category->name, $artistNames))
+				{
+						wp_delete_category($category->cat_ID);
+				}
+		}
+ }
+ 
+ 
+ /**
+  * This function hashes the API-artists and the stored categories and determines
+  * if an update is needed
+  */
+function elasticine_isCategoryUpdateNeeded($artists, $artistCategoryID)
+{
+	$APINames = array();
+	$categoryNames = array();
+	
+	//Build up arrays of just the names of each artist/category for comparison
+	//Both input arrays are sorted by artist name ascending
+	foreach($artists as $a)
+	{
+		$APINames[] = $a->name;
+	}
+	
+	$artistCategories = get_categories(array('parent' => $artistCategoryID, 'hide_empty' => 0));
+	foreach($artistCategories as $a)
+	{
+		$categoryNames[] = $a->name;
+	}
+	
+	//Right, now check the hashes to see if they're different
+	if (md5(serialize($APINames)) != md5(serialize($categoryNames)))
+	{
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/***************************************************************
+ * The following functions are for creating re-write rules for permalinks
+ *********************************************************************/
+
+add_action( 'wp_loaded','elasticine_flush_rules' );
+add_filter( 'rewrite_rules_array','elasticine_insert_rewrite_rules' );
+add_filter( 'query_vars','elasticine_insert_query_vars' );
+ 
+function elasticine_flush_rules() {
+		$rules = get_option( 'rewrite_rules' );
+		 
+		if ( ! isset( $rules['(artist)/(.+)$'] ) ) {
+				global $wp_rewrite;
+				$wp_rewrite->flush_rules();
+		}
+		
+		if ( ! isset( $rules['(shows)/(.+)$'] ) ) {
+				global $wp_rewrite;
+				$wp_rewrite->flush_rules();
+		}
+		
+		if ( ! isset( $rules['(agent)/(.+)$'] ) ) {
+				global $wp_rewrite;
+				$wp_rewrite->flush_rules();
+		}
+}
+
+function elasticine_insert_rewrite_rules( $rules ) {
+		$newrules = array();
+		$newrules['(artist)/(.+)$'] = 'index.php?pagename=$matches[1]&artist_slug=$matches[2]';
+		$newrules['(shows)/(.+)$'] = 'index.php?pagename=$matches[1]&artist_slug=$matches[2]';
+		$newrules['(agent)/(.+)$'] = 'index.php?pagename=$matches[1]&agent_slug=$matches[2]';
+		return $newrules + $rules;
+}
+
+function elasticine_insert_query_vars( $vars ) {
+ 
+		array_push($vars, 'artist_slug');
+		array_push($vars, 'agent_slug');
+		return $vars;
+}
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,94 @@
+## Synopsis
+
+This repository holds code for the Elasticine wordpress plugin and examples of usage. The Elasticine wordpress plugin can be used to integrate agency websites with our public API for automatic updating of information from the management system. 
+
+This plugin:
+1. Automatically pulls artist information from the "Public Website" section of artist set-up on Elasticine, as long as they are set to display publically on that page. 
+2. Automatically pulls announced and contracted shows for each artist on the company's roster, as long as they are set to display publically. 
+3. Maintains a set of categories, one for each public artist, as sub-categories of "artist", so that news posts can be associated with individual artists. 
+
+Artist data is set by agents in the internal Elasticine platform artist setup pages. 
+
+Artists have the following website-specific information that may be set (but may also be empty):
+1. A tagline -- a brief description of the artist
+2. A URL slug
+3. A biography, including HTML markup
+4. Info, including HTML markup -- for example, listing territories the artist plays in and information regarding logistics
+5. A set of links, with link text, a URL, a type (= web, soundcloud, facebook, or youtube), and a "featured" flag. The intention is that featured links can be displayed more prominently, or include inline media players. 
+6. A set of press assets, which will typically be images. Includes flags for display as "image" or "link", and a "featured" flag for whether the press asset should be placed more prominently. 
+7. A set of label names that the artist has released on. 
+
+## Code Examples
+
+### Retrieve all artists on roster
+```php
+
+<?php 
+/**** DATA GATHERING CODE ELASTICINE **/
+
+	do_action('elasticine_artists');
+	$artists = json_decode(get_transient('elasticine_artists'));
+
+/**** END DATA GATHERING CODE **/ 
+
+	var_dump($artists);
+?>
+
+```
+
+### Retrieve data on a single artist
+
+```php
+/**** ELASTICINE DATA GATHERING CODE **/
+$url = $_SERVER['REQUEST_URI'];
+
+//Assumes artist name slug is final part of URL
+$slug = $end = end((explode('/', rtrim($url, '/'))));
+
+do_action('elasticine_artist',$slug);
+
+$artist = json_decode(get_transient('elasticine_artist_'.$slug));
+
+//Get the next set of shows for this artist
+//We'll display the next month worth of shows  on the artist page
+$startDate = date('Y-m-d');
+$endDate = date('Y-m-d', strtotime("+3 MONTH"));
+do_action('elasticine_shows_artist', $slug, $startDate, $endDate);
+$shows = json_decode(get_transient('es_'.$slug. "_" . $startDate . "_" . $endDate));
+/****END DATA GATHERING CODE **/
+var_dump($artist);
+var_dump($shows);
+```
+
+### Retrieve shows data
+
+```php
+/**** DATA GATHERING CODE **/
+
+//Set startDate and endDate URL parameters to constrain date range of shows, using format Y-m-d. 
+
+//if no start date set, make it today
+$startDate = (array_key_exists('startDate', $_GET)) ? $_GET['startDate'] : date('Y-m-d');
+
+//if no end date set, make it one month in the future
+$endDate = (array_key_exists('endDate', $_GET)) ? $_GET['endDate'] : date('Y-m-d', strtotime("+3 MONTH"));
+
+do_action('elasticine_shows', $startDate, $endDate);
+$shows = json_decode(get_transient('es_' . $startDate . "_" . $endDate));
+
+/** END DATA GATHERING CODE **/
+
+var_dump($shows);
+```
+
+## Installation
+
+Copy the elasticine-artists.php into wp-content/plugins, and activate through Wordpress as with any other plugin. In the settings page, enter your company name as it appears in the "name" field of your company settings in Elasticine. 
+
+## Contributors
+
+Please email henry@elasticine.net if you find any bugs or have any feature requests. 
+
+## License
+
+GNU AGPLv3
